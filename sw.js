fa095632cd7d310a3c9c6f051bf3a1592eedc3bb
yethsivillalobos-blip <yethsivillalobos@gmail.com>
2026-08-17 12:39:18 -0400

    Update cache version from v3 to v4

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VILLALOBOS = 'villalobos-saas-v3';
+const CACHE_VILLALOBOS = 'villalobos-saas-v4';
 
 // Se incluyen todos los documentos HTML y librerías externas
 const recursosSaaS = [
